739cd8c2d2875ea0d9c181572648263735ed9f62
Fayzulla <fayzullarakhmatullayev@gmail.com>
2022-05-03 17:14:12 +0500

    added persistor to project for save up added cart items when page refreshes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   // applyMiddleware(...middlewares)
 );
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
