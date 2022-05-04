b67eaa949297d7fa95d18874afdcbcdbc5287d45
Fayzulla <fayzullarakhmatullayev@gmail.com>
2022-05-04 08:48:44 +0500

    removing redux dev tools from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,15 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+import { createStore } from "redux";
 import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
+let middleware;
+if (process.env.NODE_ENV === "development") {
+  middleware =
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+}
 
-const middlewares = [logger];
-
-export const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  // applyMiddleware(...middlewares)
-);
+export const store = createStore(rootReducer, middleware);
 
 export const persistor = persistStore(store);
 
